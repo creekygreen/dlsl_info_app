61b96cce42e533c5e26c7eb2bdfd51736bec00b4
creekygreen <creekygreen@gmail.com>
2024-05-28 18:01:15 +0800

    added content

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,7 +20,7 @@ class HomePage extends StatelessWidget {
           child: ClipRRect(
             borderRadius: BorderRadius.circular(8),
             child: Image.asset(
-              'assets/images/logo.png',
+              'lib/assets/images/logo.png',
               width: 300,
               height: 200,
               fit: BoxFit.contain,
@@ -38,8 +38,56 @@ class HomePage extends StatelessWidget {
   actions: const [],
   centerTitle: true,
   elevation: 2,
-
       ),
+      body: SafeArea(
+        top: true,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: MediaQuery.sizeOf(context).width,
+                height: 352,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF00872C),
+                ),
+                child: const Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 5),
+                      child: Text(
+                        'Be a Lasallian!',
+                        style: TextStyle(
+                          color: Color(0xFFFBFBFB),
+                          fontSize: 25,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.bold,
+                        )
+                      )
+                    ),
+
+                    Text(
+                      'Take the first step here. Discover your possibilities',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFFF6F6F6),
+                        fontSize: 20,
+                        letterSpacing: 0,
+                      )
+                    ),
+                    //cosdaddasdadsasddads
+
+                  ],
+                )
+              )
+            ],
+          )
+
+
+        )
+      )
+
 
 
     );
